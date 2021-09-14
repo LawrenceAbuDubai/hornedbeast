8e7ed4261d113df1fb5f0d7796df40c00a596948
Lawrence AbuDubai <EstarRave203@gmail.com>
2021-09-14 08:33:57 +0300

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react'; 
+import React from 'react';
+import './App.css';
+
 import Header from './components/Header.js';
+import About from './components/About.js'
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
-import HornedBeast from './components/HornedBeast';
+
+import SelectedBeast from './components/SelectedBeast.js'
+
+import data from './data.json'
+
 
 class App extends React.Component {
-  render(){
-    return(
-    <div>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayModal: false,
+      lilBeastieData: data,
+      selectedBeast: {}
+    }
+  }
+
+  showModal = (name) => {
+
+    const selectedBeast = data.find(beast => beast.title === name);
+    this.setState({selectedBeast, displayModal:true});
+  }
+
+  hideModal = () => {this.setState({displayModal: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <About />
+        <Main 
+          showModal={this.showModal}
+          beasts={this.state.lilBeastieData} 
+        />
+        <SelectedBeast 
+          displayModal={this.state.displayModal}
+          hideModal={this.hideModal}
+          selectedBeast={this.state.selectedBeast}
+        />
+        <Footer />
+      </div> 
     )
   }
 }
